17108464ac663d691820cf2e0cddc62c513fbf15
Gabriel Rios <gabrielfalcaorios@gmail.com>
2015-07-17 17:16:03 -0300

    Add canvas renderer

--- lib/opal/pixi/canvas_renderer.rb
+++ lib/opal/pixi/canvas_renderer.rb
@@ -1,19 +1,16 @@
 module PIXI
-  class WebGLRenderer  #< `PIXI.WebGLRenderer`
+  class CanvasRenderer
     include Native
 
     def initialize(w_or_native, height, options)
       if native?(w_or_native)
         super(w_or_native)
       else
-        super(`new PIXI.WebGLRenderer(w_or_native, height , #{ options.to_n })`)
+        super(`new PIXI.CanvasRenderer(w_or_native, height, #{ options.to_n })`)
       end
     end
 
-    alias_native :render
-    alias_native :view
     alias_native :width
     alias_native :height
-
   end
 end
