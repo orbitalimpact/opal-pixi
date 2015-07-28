09af67bd5990b63007e66988f13ac94a72b999cc
George D. Plymale II <georgedp@orbitalimpact.com>
2015-07-28 15:48:36 -0400

    move changes from opal-phaser's Sprite class to here

--- lib/opal/pixi/sprite.rb
+++ lib/opal/pixi/sprite.rb
@@ -13,6 +13,32 @@ module PIXI
     alias_native :anchor=
     alias_native :position=
     alias_native :rotation=
+    
+    alias_native :x
+    alias_native :y
+    alias_native :x=
+    alias_native :y=
+    alias_native :z
+    alias_native :z=
+    
+    alias_native :height
+    alias_native :width
+    
+    alias_native :scale
+    alias_native :visible=
+    
+    alias_native :kill
+    alias_native :destroy
+    
+    alias_native :load_texture, :loadTexture
+    
+    def crop(rect)
+      `#@native.crop(#{rect.to_n})`
+    end
+    
+    def smoothed=(bool)
+      `#@native.smoothed = bool`
+    end
 
     %x{
       self._proto = window.PIXI.Sprite.prototype, def = self._proto;
